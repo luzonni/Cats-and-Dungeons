97c16f3e3e02ecc09eb3efb2f8c380986d0ffc01
Luzonni <lucas.z.200375@gmail.com>
2025-02-28 12:12:15 -0300

    change sprites and change wave start

--- src/com/retronova/game/map/Waves.java
+++ src/com/retronova/game/map/Waves.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 public class Waves {
 
-    private int wave = 20;
+    private int wave = 1;
     private double waveMultiplier = 1;
     private int counter;
     private int lastCounter;
