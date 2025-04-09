2c2915f3222301debc013b5ccef01db05a4d8e93
Luzonni <lucas.z.200375@gmail.com>
2025-04-08 21:17:47 -0300

    consertando particula walking e som de andar

--- src/com/retronova/game/hud/WaveDisplay.java
+++ src/com/retronova/game/hud/WaveDisplay.java
@@ -6,6 +6,7 @@ import com.retronova.engine.graphics.FontG;
 import com.retronova.engine.graphics.SpriteSheet;
 import com.retronova.game.Game;
 import com.retronova.game.map.arena.Arena;
+import com.retronova.game.objects.entities.enemies.Enemy;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -14,6 +15,7 @@ public class WaveDisplay {
 
     private final BufferedImage clock;
     private final Point position;
+    private int amountOfEnemies;
 
     WaveDisplay() {
         this.clock = new SpriteSheet("ui", "clock", Configs.HudScale()).getSHEET();
@@ -22,6 +24,7 @@ public class WaveDisplay {
 
     public void tick() {
         this.position.setLocation(Engine.window.getWidth()/2, Configs.Margin());
+        this.amountOfEnemies = Game.getMap().getEntities(Enemy.class).size();
     }
 
     public void render(Graphics2D g) {
@@ -50,21 +53,29 @@ public class WaveDisplay {
         Font font = FontG.font(FontG.Septem, Configs.HudScale()*6);
         g.setFont(font);
 
-        int xLevel = x + 15 * Configs.HudScale();
-        int yLevel = y + 6 * Configs.HudScale();
-        String level = "Lvl: " + Game.getGame().getLevel();
+        int xLevel = x + 18 * Configs.HudScale();
+        int yLevel = y + 7 * Configs.HudScale();
+        String level = "Level: " + (Game.getGame().getLevel()+1);
         g.setColor(new Color(0x872341));
         g.drawString(level, xLevel + Configs.HudScale(), yLevel + Configs.HudScale());
         g.setColor(Color.white);
         g.drawString(level, xLevel, yLevel);
 
-        int xCount = x + 15 * Configs.HudScale();
-        int yCount = y + 11 * Configs.HudScale();
-        String count = "Spw: " + Game.getWave().currentSpawn() + " / " + Game.getWave().amountSpawns();
+        int xCount = x + 18 * Configs.HudScale();
+        int yCount = y + 13 * Configs.HudScale();
+        String count = "Spawns: " + Game.getWave().currentSpawn() + " / " + Game.getWave().amountSpawns();
         g.setColor(new Color(0x872341));
         g.drawString(count, xCount + Configs.HudScale(), yCount + Configs.HudScale());
         g.setColor(Color.white);
         g.drawString(count, xCount, yCount);
+
+        int xAmount = x + 18 * Configs.HudScale();
+        int yAmount = y + 19 * Configs.HudScale();
+        String amount = "Enemies: " + this.amountOfEnemies;
+        g.setColor(new Color(0x872341));
+        g.drawString(amount, xAmount + Configs.HudScale(), yAmount + Configs.HudScale());
+        g.setColor(Color.white);
+        g.drawString(amount, xAmount, yAmount);
 
 
     }
