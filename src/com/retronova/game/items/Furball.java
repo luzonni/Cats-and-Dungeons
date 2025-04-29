91954fc695d7595419430bb6a2ca1d7eedd1cb0f
CaioRenatoDot <caiorenato146@gmail.com>
2025-04-29 16:01:06 -0300

    feat: novo item, furball ou bola de pelo. ainda em criação

---
+++ src/com/retronova/game/items/Furball.java
@@ -0,0 +1,21 @@
+package com.retronova.game.items;
+
+import java.awt.*;
+
+public class Furball extends Item {
+// A ideia é um item que é defensivo, e ele vai atirar 6 bolas de pelos, partindo do personagem.
+
+    Furball(int id) {
+        super(id, "Furball", "sprite");
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics2D g) {
+
+    }
+}
