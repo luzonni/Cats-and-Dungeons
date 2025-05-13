17305b7fce839aafd9303411be5e31f704191645
Luzonni <lucas.z.200375@gmail.com>
2025-05-13 10:14:58 -0300

    fix: add main o main package

---
+++ src/com/retronova/Main.java
@@ -0,0 +1,9 @@
+package com.retronova;
+
+import com.retronova.engine.Engine;
+
+public class Main {
+    public static void main(String[] args) {
+        new Engine();
+    }
+}
--- src/com/retronova/engine/Main.java
+++
@@ -1,9 +0,0 @@
-package com.retronova.engine;
-
-public class Main {
-
-    public static void main(String[] args) {
-        new Engine();
-    }
-
-}
