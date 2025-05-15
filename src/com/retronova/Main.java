6ff2668d6d1fde79bc18489188a1d51f473d9875
luzonni <lucas.z.200375@gmail.com>
2025-05-14 21:31:28 -0300

    FIX: A PORRA DA ESCADA DO WINDOWS FILHO DUMA PUTA

--- src/com/retronova/Main.java
+++ src/com/retronova/Main.java
@@ -6,7 +6,10 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        DPIFix.fixDPI();
+        String os = System.getProperty("os.name").toLowerCase();
+        if(os.contains("win")) {
+            System.setProperty("sun.java2d.uiScale", "1.0");
+        }
         SwingUtilities.invokeLater(Engine::new);
     }
 }
